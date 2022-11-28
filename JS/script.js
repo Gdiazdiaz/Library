d02c18a742d547ca5c94144901034d6abfe6840b
Glenda Diaz <glendadiazah@outlook.com>
2022-11-28 14:54:04 -0600

    Add books section, input section, style, def vars in JS

--- JS/script.js
+++
@@ -1,4 +0,0 @@
-const addButton = document.querySelector('.form-button');
-const removeButton = document.querySelector('remove-book');
-var bookTitle = document.getElementById('book-title').value;
-var bookAuthor = document.getElementById('book-author').value;
