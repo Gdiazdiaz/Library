34207e088aa6a37367059d4446fb1f41f889ed49
Glenda Diaz <glendadiazah@outlook.com>
2022-11-28 17:41:45 -0600

    add JS to add a book to the array

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,51 @@
 const addButton = document.querySelector('.form-button');
 const removeButton = document.querySelector('remove-book');
-var bookTitle = document.getElementById('book-title').value;
-var bookAuthor = document.getElementById('book-author').value;
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector('#book-author');
+const library = document.querySelector('.book-main-container');
 
-const listBooks = [];
 
-function Book(name, author) {
-    this.name = name;
-    this.author = author;
-  }
+function addBook(){
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    
+    const book = {
+        title: title,
+        author: author,
+    }
 
-// addButton.addEventListener('click', (bookTitle, bookAuthor) => {
+    const listBooks = [];
 
-    listBooks[0] = new Book('BookName', 'bookAutho');
-
+    listBooks.push(book);
     console.log(listBooks);
 
-    listBooks[1] = new Book('BookNamse', 'bookAsssutho');
+    let bookgenerator = '';
 
-    console.log(listBooks);
+listBooks.forEach((book) => {
+  bookgenerator += `<div class="book-container">
+  <h2 class="book-title">${book.title}</h2>
+  <h3 class="book-author">${book.author}</h3>
+  <button class="remove-book">Remove</button>
+</div>`;
+});
+library.innerHTML += bookgenerator;
+}
+addButton.addEventListener('click', addBook);
+//function Book(name, author) {
+//    this.name = name;
+ //   this.author = author;
+ // }
+
+  
+
+//addButton.addEventListener('click', (bookTitle, bookAuthor) => {
+
+ //   listBooks[0] = new Book('BookName', 'bookAutho');
+
+ //  console.log(listBooks);
+
+ //   listBooks[1] = new Book('BookNamse', 'bookAsssutho');
+
+ //  console.log(listBooks);
 
 // })
