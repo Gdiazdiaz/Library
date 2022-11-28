daf246a0a6f1959188210ebf58dbdb817a164fa6
diego <dgcuenca@gmail.com>
2022-11-28 16:45:44 -0500

    Use a constructor to add information to an Array

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,22 @@ const addButton = document.querySelector('.form-button');
 const removeButton = document.querySelector('remove-book');
 var bookTitle = document.getElementById('book-title').value;
 var bookAuthor = document.getElementById('book-author').value;
+
+const listBooks = [];
+
+function Book(name, author) {
+    this.name = name;
+    this.author = author;
+  }
+
+// addButton.addEventListener('click', (bookTitle, bookAuthor) => {
+
+    listBooks[0] = new Book('BookName', 'bookAutho');
+
+    console.log(listBooks);
+
+    listBooks[1] = new Book('BookNamse', 'bookAsssutho');
+
+    console.log(listBooks);
+
+// })
