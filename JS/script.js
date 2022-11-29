459d0de0b4decb3c95e963ff55a6bff1ee9c7bd8
Glenda Diaz <glendadiazah@outlook.com>
2022-11-28 22:54:04 -0600

    Set remove function to work

--- JS/script.js
+++ JS/script.js
@@ -8,14 +8,6 @@ const listBooks = JSON.parse(localStorage.getItem('listBooks')) || [];
 function addBook(){
     let title = bookTitle.value;
     let author = bookAuthor.value;
- //   let id= 0;
-  //  if(JSON.parse(localStorage.getItem('listBooks'))==null){
- //       id = 0;
-  //  }
-  //  else {
- //       listBooks = JSON.parse(localStorage.getItem('listBooks'));
- //       id = listBooks.length;
- //   }
     if(!title || !author){
         alert('please fill all information');
     }
@@ -46,7 +38,7 @@ function addBook(){
 }
 window.onload = () => {
     let bookgenerator = '';
-    listBooks.forEach((book, index) => {
+    listBooks.forEach((book) => {
     bookgenerator += `<div class="book-container">
     <h2 class="book-title">${book.title}</h2>
     <h3 class="book-author">${book.author}</h3>
@@ -55,27 +47,9 @@ window.onload = () => {
   });
   library.innerHTML = bookgenerator;};
 function removeBook(title){
- //   for(var i =0;i < listBooks.length;i++){
-  //      if(listBooks[i].title == title){
- //         bookmarks.splice(i, 1);
-  //      }
-  //    }
-    const resultBooks = listBooks.filter(book => book.title !== title.id);
-   localStorage.setItem('listBooks', JSON.stringify(resultBooks));
-   addBook();
-  console.log(title.id);
+const resultBooks = listBooks.filter(book => book.title !== title.id);
+localStorage.setItem('listBooks', JSON.stringify(resultBooks));
+window.location.reload();
 }
 
 addButton.addEventListener('click', addBook);
-//if(JSON.parse(localStorage.getItem('listBooks'))==null){}
-//else{
-//let bookgenerator = '';
- //   listBooks.forEach((book) => {
- //           bookgenerator += `<div class="book-container">
- //           <h2 class="book-title">${book.title}</h2>
-  //          <h3 class="book-author">${book.author}</h3>
-  //          <button class="remove-book" onClick=removeBook()>Remove</button>
-   //       </div>`;
-  //        });
-//library.innerHTML = bookgenerator;
- //       }
