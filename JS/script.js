60d526396bb93a7d9f1665fab2a5406087435f14
diego <dgcuenca@gmail.com>
2022-11-29 00:21:12 -0500

    Fix linters erros

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,54 @@
 const addButton = document.querySelector('.form-button');
-const removeButton = document.querySelector('remove-book');
-const bookTitle = document.querySelector("#book-title");
+const bookTitle = document.querySelector('#book-title');
 const bookAuthor = document.querySelector('#book-author');
 const library = document.querySelector('.book-main-container');
 const listBooks = JSON.parse(localStorage.getItem('listBooks')) || [];
 
-function addBook(){
-    let title = bookTitle.value;
-    let author = bookAuthor.value;
-    if(!title || !author){
-        alert('please fill all information');
-    }
-    else{
+function addBook() {
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  if (!title || !author) {
+    alert('please fill all information');
+  } else {
     const book = {
-        title: title,
-        author: author,
-     //   id: id,
-    }
-    if(JSON.parse(localStorage.getItem('listBooks'))==null){
-        listBooks.push(book);
-        localStorage.setItem('listBooks', JSON.stringify(listBooks));
-    }
-    else {
-        listBooks.push(book);
-        localStorage.setItem('listBooks', JSON.stringify(listBooks));
+      title,
+      author,
+      //   id: id,
+    };
+    if (JSON.parse(localStorage.getItem('listBooks')) == null) {
+      listBooks.push(book);
+      localStorage.setItem('listBooks', JSON.stringify(listBooks));
+    } else {
+      listBooks.push(book);
+      localStorage.setItem('listBooks', JSON.stringify(listBooks));
     }
     let bookgenerator = '';
     listBooks.forEach((book) => {
-            bookgenerator += `<div class="book-container">
+      bookgenerator += `<div class="book-container">
             <h2 class="book-title">${book.title}</h2>
             <h3 class="book-author">${book.author}</h3>
             <button class="remove-book" id="${book.title}" onClick=removeBook(this)>Remove</button>
           </div>`;
-          });
-          library.innerHTML = bookgenerator;
-        }
+    });
+    library.innerHTML = bookgenerator;
+  }
 }
 window.onload = () => {
-    let bookgenerator = '';
-    listBooks.forEach((book) => {
+  let bookgenerator = '';
+  listBooks.forEach((book) => {
     bookgenerator += `<div class="book-container">
     <h2 class="book-title">${book.title}</h2>
     <h3 class="book-author">${book.author}</h3>
     <button class="remove-book" id="${book.title}" onClick=removeBook(this)>Remove</button>
   </div>`;
   });
-  library.innerHTML = bookgenerator;};
-function removeBook(title){
-const resultBooks = listBooks.filter(book => book.title !== title.id);
-localStorage.setItem('listBooks', JSON.stringify(resultBooks));
-window.location.reload();
+  library.innerHTML = bookgenerator;
+};
+
+function removeBook(title) {
+  const resultBooks = listBooks.filter((book) => book.title !== title.id);
+  localStorage.setItem('listBooks', JSON.stringify(resultBooks));
+  window.location.reload();
 }
 
 addButton.addEventListener('click', addBook);
