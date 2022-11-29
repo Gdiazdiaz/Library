c1c8476436befd21ec93d5929aec260007504e74
diego <dgcuenca@gmail.com>
2022-11-28 20:46:45 -0500

    Manage data with local storage

--- JS/script.js
+++ JS/script.js
@@ -3,49 +3,48 @@ const removeButton = document.querySelector('remove-book');
 const bookTitle = document.querySelector("#book-title");
 const bookAuthor = document.querySelector('#book-author');
 const library = document.querySelector('.book-main-container');
-
-
 function addBook(){
     let title = bookTitle.value;
     let author = bookAuthor.value;
-    
+    if(!title || !author){
+        alert('please fill all information');
+    }
+    else{
     const book = {
         title: title,
         author: author,
     }
-
-    const listBooks = [];
-
-    listBooks.push(book);
-    console.log(listBooks);
-
+    if(JSON.parse(localStorage.getItem('listBooks'))==null){
+        const listBooks=[];
+        listBooks.push(book);
+        localStorage.setItem('listBooks', JSON.stringify(listBooks));
+    }
+    else {
+        const listBooks = JSON.parse(localStorage.getItem('listBooks'));
+        listBooks.push(book);
+        localStorage.setItem('listBooks', JSON.stringify(listBooks));
+    }
+    const listBooks = JSON.parse(localStorage.getItem('listBooks'));
     let bookgenerator = '';
-
-listBooks.forEach((book) => {
-  bookgenerator += `<div class="book-container">
-  <h2 class="book-title">${book.title}</h2>
-  <h3 class="book-author">${book.author}</h3>
-  <button class="remove-book">Remove</button>
-</div>`;
-});
-library.innerHTML += bookgenerator;
+    listBooks.forEach((book) => {
+            bookgenerator += `<div class="book-container">
+            <h2 class="book-title">${book.title}</h2>
+            <h3 class="book-author">${book.author}</h3>
+            <button class="remove-book">Remove</button>
+          </div>`;
+          });
+          library.innerHTML = bookgenerator;
+          console.log(listBooks);
+        }
 }
 addButton.addEventListener('click', addBook);
-//function Book(name, author) {
-//    this.name = name;
- //   this.author = author;
- // }
-
-  
-
-//addButton.addEventListener('click', (bookTitle, bookAuthor) => {
-
- //   listBooks[0] = new Book('BookName', 'bookAutho');
-
- //  console.log(listBooks);
-
- //   listBooks[1] = new Book('BookNamse', 'bookAsssutho');
-
- //  console.log(listBooks);
-
-// })
+const listBooks = JSON.parse(localStorage.getItem('listBooks'));
+let bookgenerator = '';
+    listBooks.forEach((book) => {
+            bookgenerator += `<div class="book-container">
+            <h2 class="book-title">${book.title}</h2>
+            <h3 class="book-author">${book.author}</h3>
+            <button class="remove-book">Remove</button>
+          </div>`;
+          });
+library.innerHTML = bookgenerator;
